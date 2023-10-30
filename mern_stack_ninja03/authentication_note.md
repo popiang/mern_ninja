464f51d2eba6f3af6d9310f95da2327eacb94837
SHAHRIL MAD SHAH <popiang@hotmail.com>
2023-10-31 07:10:52 +0800

    asdf

---
+++ mern_stack_ninja03/authentication_note.md
@@ -0,0 +1,13 @@
+steps
+1. create route
+2. create controller
+3. bring routes to server.js
+4. create model for user
+5. create static method for signup in user
+	- check if user already exist
+	- install bcrypt and hash the password
+	- save user
+6. install validator and do validation
+	- check if email and password are provided
+	- check if email is valid using validator
+	- check if password is strong enough using validator
--- mern_stack_ninja03/backend/controllers/userController.js
+++ mern_stack_ninja03/backend/controllers/userController.js
@@ -1,13 +1,26 @@
-exports.signup = (req, res) => {
-    res.status(200).json({
-        status: "Success",
-        message: "Signup user",
-    });
+const User = require("../models/userModel");
+
+exports.signup = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.signup(email, password);
+
+        res.status(200).json({
+            status: "Success",
+            user: user,
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: "Fail",
+            message: error.message,
+        });
+    }
 };
 
 exports.login = (req, res) => {
     res.status(200).json({
         status: "Success",
-        message: "Loginuser",
+        message: "Login user",
     });
 };
